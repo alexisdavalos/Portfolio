abe6a63a98804659789c4292b71feacaf5407d36
Alexis <alexisdavalos.tech@gmail.com>
2020-03-07 21:26:20 -0500

    Reformatted href and class attributes in Nav.js

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,59 +2,59 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a href='#top' className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt= 'logo' width="112" height="28" />
                 </a>
 
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a href='#top' role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item">
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <a href='#top' className="navbar-item" >
                         Home
             </a>
 
-                    <a class="navbar-item">
+                    <a href='#top' className="navbar-item">
                         Documentation
             </a>
 
-                    <div class="navbar-item has-dropdown is-hoverable">
-                        <a class="navbar-link">
+                    <div className="navbar-item has-dropdown is-hoverable">
+                        <a href='#top' className="navbar-link">
                             More
                 </a>
 
-                        <div class="navbar-dropdown">
-                            <a class="navbar-item">
+                        <div className="navbar-dropdown">
+                            <a href='#top' className="navbar-item">
                                 About
                 </a>
-                            <a class="navbar-item">
+                            <a href='#top' className="navbar-item">
                                 Jobs
                 </a>
-                            <a class="navbar-item">
+                            <a href='#top' className="navbar-item">
                                 Contact
                 </a>
-                            <hr class="navbar-divider" />
-                            <a class="navbar-item">
+                            <hr className="navbar-divider" />
+                            <a href='#top' className="navbar-item">
                                 Report an issue
                 </a>
                         </div>
                     </div>
                 </div>
 
-                <div class="navbar-end">
-                    <div class="navbar-item">
-                        <div class="buttons">
-                            <a class="button is-primary">
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a href='#top' className="button is-primary">
                                 <strong>Sign up</strong>
                             </a>
-                            <a class="button is-light">
+                            <a href='#top' className="button is-light">
                                 Log in
                 </a>
                         </div>
