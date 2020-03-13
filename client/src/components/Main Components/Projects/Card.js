199568c92d73bd4d6eab69db02464a5c7b74a3b3
Alexis <alexisdavalos.tech@gmail.com>
2020-03-13 01:12:23 -0400

    Added Grid, Need To Work On Hover

--- client/src/components/Main Components/Projects/Card.js
+++ client/src/components/Main Components/Projects/Card.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Card = ({project}) =>{
     return(
-        <div className="column is-one-quarter">
+        <div className="column is-one-third">
         <div className="card">
+        <div className="card-image">
+            <figure className="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="project"/>
+            </figure>
+        </div>
         <div className="card-content has-background-dark has-text-white-ter">
             <div className="content">
             {project.description}
@@ -13,11 +18,6 @@ const Card = ({project}) =>{
             <br/>
             <time dateTime="2016-1-1">11:09 PM - 1 Jan 2020</time>
             </div>
-        </div>
-        <div className="card-image">
-            <figure className="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="project"/>
-            </figure>
         </div>
         </div>
 </div>
