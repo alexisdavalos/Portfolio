19d0be3f83b2a5aa0df486854b1f0487bd74fc88
Alexis <alexisdavalos.tech@gmail.com>
2020-03-13 01:21:53 -0400

    Styling Added

--- client/src/components/Main Components/Projects/Card.js
+++ client/src/components/Main Components/Projects/Card.js
@@ -4,11 +4,6 @@ const Card = ({project}) =>{
     return(
         <div className="column is-one-third">
         <div className="card">
-        <div className="card-image">
-            <figure className="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="project"/>
-            </figure>
-        </div>
         <div className="card-content has-background-dark has-text-white-ter">
             <div className="content">
             {project.description}
@@ -18,6 +13,11 @@ const Card = ({project}) =>{
             <br/>
             <time dateTime="2016-1-1">11:09 PM - 1 Jan 2020</time>
             </div>
+        </div>
+        <div className="card-image">
+            <figure className="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="project"/>
+            </figure>
         </div>
         </div>
 </div>
