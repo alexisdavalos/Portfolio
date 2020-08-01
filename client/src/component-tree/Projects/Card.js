73eb1dbd6678216d3ee11ddbe5a097c90af94763
alexisdavalos <alexisdavalos.tech@gmail.com>
2020-08-01 11:36:48 -0400

    error cleanups + css safari compatability

--- client/src/component-tree/Projects/Card.js
+++ client/src/component-tree/Projects/Card.js
@@ -17,7 +17,7 @@ const Card = ({ project, darkMode }) => {
       {/* Card Component */}
       <div className="card" onClick={() => setModal(!modal)}>
         <div className="card-image is-square">
-          <button class="button is-light is-rounded is-outlined">
+          <button className="button is-light is-rounded is-outlined">
             Learn More
           </button>
           <img src={project.img} alt={project.description} />
